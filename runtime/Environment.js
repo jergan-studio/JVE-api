2baa9ceb3a5c11ca9e8f806499a5fb44906a6518
jergan-studio <jose343lopez22@gmail.com>
2026-05-30 09:35:30 -0500

    Update Environment.js

--- runtime/Environment.js
+++ runtime/Environment.js
@@ -1,18 +1,30 @@
 class Environment {
   constructor() {
-    this.vars = new Map();
+    this.variables = new Map();
   }
 
   set(name, value) {
-    this.vars.set(name, value);
+    this.variables.set(name, value);
   }
 
   get(name) {
-    return this.vars.get(name);
+    return this.variables.get(name);
   }
 
   has(name) {
-    return this.vars.has(name);
+    return this.variables.has(name);
+  }
+
+  delete(name) {
+    this.variables.delete(name);
+  }
+
+  clear() {
+    this.variables.clear();
+  }
+
+  dump() {
+    return Object.fromEntries(this.variables);
   }
 }
 
