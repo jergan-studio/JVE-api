3505d42ae59b31bfa6f9e9dbe7d15d88aeefe306
jergan-studio <jose343lopez22@gmail.com>
2026-05-30 09:38:34 -0500

    Update FileRunner.js

--- runtime/FileRunner.js
+++ runtime/FileRunner.js
@@ -1,12 +1,27 @@
 const fs = require("fs");
+const path = require("path");
 
 class FileRunner {
   constructor(scriptEngine) {
     this.scriptEngine = scriptEngine;
   }
 
-  runFile(path) {
-    const script = fs.readFileSync(path, "utf-8");
+  run(filePath) {
+    const absolute =
+      path.resolve(filePath);
+
+    if (!fs.existsSync(absolute)) {
+      throw new Error(
+        `Script not found: ${absolute}`
+      );
+    }
+
+    const script =
+      fs.readFileSync(
+        absolute,
+        "utf8"
+      );
+
     this.scriptEngine.run(script);
   }
 }
