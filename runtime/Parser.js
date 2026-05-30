0a84c9873110be32cae6af9e775f28b3ec9aa886
jergan-studio <jose343lopez22@gmail.com>
2026-05-30 09:35:56 -0500

    Update Parser.js

--- runtime/Parser.js
+++ runtime/Parser.js
@@ -1,19 +1,31 @@
 class Parser {
   parse(script) {
-    const lines = script.split("\n").map(l => l.trim()).filter(Boolean);
+    const lines = script.split("\n");
 
-    return lines.map(line => {
-      const [command, ...args] = this.tokenize(line);
+    const instructions = [];
 
-      return {
-        command,
-        args
-      };
-    });
+    for (let lineNumber = 0; lineNumber < lines.length; lineNumber++) {
+      const raw = lines[lineNumber].trim();
+
+      // Ignore blanks
+      if (!raw) continue;
+
+      // Ignore comments
+      if (raw.startsWith("#")) continue;
+
+      instructions.push({
+        line: lineNumber + 1,
+        raw,
+        tokens: this.tokenize(raw)
+      });
+    }
+
+    return instructions;
   }
 
   tokenize(line) {
     const regex = /"([^"]*)"|(\S+)/g;
+
     const tokens = [];
     let match;
 
